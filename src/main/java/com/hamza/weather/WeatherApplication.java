5f37015089f09c02a966a58e53498c4e53d50490
HamzahZuhair1 <hmzhzuhair@gmail.com>
2026-07-13 15:34:29 +0300

    add initial spring boot project files

---
+++ src/main/java/com/hamza/weather/WeatherApplication.java
@@ -0,0 +1,15 @@
+package com.hamza.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+
+		System.out.println("Test");
+	}
+
+}
